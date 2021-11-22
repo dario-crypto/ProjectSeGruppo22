a143033ce601eddbc2f6a7e022e2640f72ab67b4
Diletta <d.vitale25@studenti.unisa.it>
2021-11-22 16:05:52 +0100

    Calculator,ComplexNumber e VariablesSpace

---
+++ Project/src/main/java/com/mycompany/project/Calculator.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project;
+
+/**
+ *
+ * @author dilet
+ */
+public class Calculator {
+    private StackLogic<ComplexNumber> stack;
+    private VariablesSpace variablesSpace;
+    
+    public Calculator(StackLogic stack){
+        this.stack=stack;
+        //this.variablesSpace=variablesSpace;
+    }
+    
+    public void insert(ComplexNumber c){
+        stack.push(c);
+        
+    }
+    
+    public ComplexNumber sum(){
+        return null;
+    }
+    
+    public ComplexNumber sub(){
+        return null;
+    }
+    
+    public ComplexNumber divide(){
+        return null;
+    }
+    
+    public ComplexNumber prod(){
+        return null;
+    }
+    
+    public ComplexNumber square(){
+        return null;
+    }
+    
+    public ComplexNumber invertSign(){
+        return null;
+    }
+    
+    public ComplexNumber execOperation(String op){
+        return null;
+    }
+    
+    public void clear(){
+        
+    }
+    
+    public void drop(){
+        
+    }
+    
+    public void dup(){
+        
+    }
+    
+    public void swap(){
+        
+    }
+    
+    public void over(){
+        
+    }
+    
+    public void saveToVariable(String name){
+        
+    }
+    
+    public void saveToStack(String name){
+        
+    }
+    
+    public void addToVariable(String name){
+        
+    }
+    
+    public void subToVariable(String name){
+        
+    }
+}
---
+++ Project/src/test/java/com/mycompany/project/CalculatorTest.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.mycompany.project;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dilet
+ */
+public class CalculatorTest {
+    Calculator calculator;
+    
+    
+    public CalculatorTest() {
+    }
+    
+    
+    
+    @Before
+    public void setUp() {
+        calculator=new Calculator(new Stack<ComplexNumber>());
+        
+    
+        
+    
+        
+    }
+    
+    
+
+    
+    
+}
