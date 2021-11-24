1536769c940e89d09e6209ff42ab26b33a0e9e2d
Diletta <d.vitale25@studenti.unisa.it>
2021-11-24 11:36:20 +0100

    add e prod e insert

--- Project/src/main/java/com/mycompany/project/NewMain.java
+++ Project/src/main/java/com/mycompany/project/NewMain.java
@@ -13,7 +13,7 @@ public class NewMain {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws StackEmptyException {
+    public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
         System.out.println("Sono Diletta");
