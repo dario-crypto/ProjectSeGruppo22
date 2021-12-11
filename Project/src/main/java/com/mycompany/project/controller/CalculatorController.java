1b2250cef9e9f55029873b4f8524eb2044e14b49
user <user@lenovo>
2021-12-11 11:56:30 +0100

    case sensitive isOperationVariable

--- Project/src/main/java/com/mycompany/project/controller/CalculatorController.java
+++ Project/src/main/java/com/mycompany/project/controller/CalculatorController.java
@@ -109,8 +109,8 @@ public class CalculatorController {
      * @param op string that represents an operation
      * @return true if it is an operation on variables, otherwise false
      */
-    private boolean isOperationVariable(String op) {
-        Pattern pattern = Pattern.compile("^[< \\+ \\- >]{1}[a-z]{1}$", Pattern.CASE_INSENSITIVE);
+    public static boolean isOperationVariable(String op) {
+        Pattern pattern = Pattern.compile("^[< \\+ \\- >]{1}[a-z]{1}$");
         Matcher matcher = pattern.matcher(op);
         return matcher.find();
     }
@@ -287,6 +287,9 @@ public class CalculatorController {
     public void saveFile() throws IOException{
         formulas.save();
     }
+    
+    
+  
     
 
 }
